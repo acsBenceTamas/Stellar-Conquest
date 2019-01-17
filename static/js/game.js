a72d562268a7195cab2fb6848b0b148661b7ec51
acsBenceTamas <acs.bence.tamas@gmail.com>
2019-01-17 16:54:08 +0100

    Implemented move phase

--- static/js/game.js
+++ static/js/game.js
@@ -80,10 +80,10 @@ function connectPoints( point1, point2, thickness) {
     let angle = Math.atan2((point1.y-point2.y),(point1.x-point2.x))*(180/Math.PI);
     // make hr
     let connectionLine = document.createElement('div');
-    connectionLine.classList.add('connection-line', 'selectable');
+    connectionLine.classList.add('connection-line', 'selectable', 'game-piece');
     connectionLine.style=
         `padding:0;
-        margin:0; height:${thickness}px;
+        margin:0;
         line-height:1px;
         position:absolute;
         left:${cx}px;
@@ -100,7 +100,7 @@ function connectPoints( point1, point2, thickness) {
 
 function createNode( posX, posY, width, height, parent, id) {
     let node = document.createElement('div');
-    node.classList.add('game-node', 'selectable');
+    node.classList.add('game-node', 'selectable', 'game-piece');
     node.id = id;
     node.style = `left: ${posX-width/2}px; top: ${posY-height}px; height:${height}px; width:${width}px;`;
     node.dataset.connections = JSON.stringify([]);
@@ -123,7 +123,7 @@ function makeNodeConnection( node1, node2) {
 
 function createShip( nodeId, shipConfiguration, shipId) {
     let ship = document.createElement('img');
-    ship.classList.add('game-ship', 'selectable');
+    ship.classList.add('game-ship', 'selectable', 'game-piece');
     ship.id = shipId;
     ship.src = emblemsFolder + shipConfiguration.image;
     ship.dataset.name = shipConfiguration.name;
@@ -136,37 +136,129 @@ function getAllConnectionsForNode( node ) {
     return Array.from(document.getElementsByClassName('connection-line')).filter(el => JSON.parse(el.dataset.nodes).includes(node.id));
 }
 
-function clickedGameBoardElement( event ) {
-    let target = event.target;
+function clickUpdate() {
     for (let selectable of document.getElementsByClassName('selectable')) {
         selectable.classList.remove('selected');
-    }
-    if ( isGameNode(target) ) {
-        target.classList.add('selected');
-        for (let connectionLine of getAllConnectionsForNode(event.target)){
-            connectionLine.classList.add('selected');
+        if (selectable.classList.contains('connection-line') || selectable.classList.contains('game-node')) {
+            selectable.classList.remove('travelled', 'enemy');
         }
-    }
-    if ( isFriendlyShip(target) ) {
-        target.classList.add('selected');
-    }
-    if ( isEnemyShip(target) ) {
-        target.classList.add('selected', 'enemy');
     }
 }
 
-function startOfTurnCleanUp() {
-    for (let ship of document.getElementsByClassName('game-ship')) {
-        if (isEnemyShip(ship)) {
-            ship.classList.add('enemy');
+function updateGameInfo() {
+    let gameInfo = document.getElementById('game-info');
+    gameInfo.textContent = `Current player: ${currentPlayer} | Available ships: ${countFriendlyShips()} | Ships without order: ${countFriendlyShipsWithoutOrder()}`
+}
+
+function countFriendlyShips() {
+    return Array.from(document.getElementsByClassName('game-ship')).filter(
+        ship => ship.dataset.owner == currentPlayer
+    ).length
+}
+
+function countFriendlyShipsWithoutOrder() {
+    return Array.from(document.getElementsByClassName('game-ship')).filter(
+        ship => ship.dataset.owner == currentPlayer && !ship.classList.contains('travelled') && ship.dataset.type != "station"
+    ).length
+}
+
+function clickedGameBoardElement( event ) {
+    let clickTarget = event.target;
+    clickUpdate();
+    if ( isShip(clickTarget) ) {
+        if (currentShip == clickTarget) {
+            deselectShip()
         } else {
-            ship.classList.remove('enemy');
+            selectShip(clickTarget);
+        }
+    } else if ( isGameNode(clickTarget) ) {
+        if (shipIsSelected()) {
+            if (isFriendlyShip(currentShip) && canMoveTo(currentShip, clickTarget)) {
+                if (currentShip.parentNode.id == clickTarget.id || currentShip.dataset.moveTarget == clickTarget.id) {
+                    cancelMoveOrder(currentShip);
+                } else {
+                    issueMoveOrder(currentShip, clickTarget);
+                }
+                selectShip(currentShip);
+            } else {
+                selectNode( clickTarget );
+                deselectShip();
+            }
+        } else {
+            selectNode( clickTarget );
         }
     }
+    updateGameInfo()
+}
+
+function deselectShip() {
+    currentShip = null;
+    for (let node of document.getElementsByClassName('game-node travelled')) {
+        node.classList.remove('travelled');
+    }
+}
+
+function canMoveTo( ship, targetNode ) {
+     return (Array.from(document.getElementsByClassName('connection-line')).filter(
+        line => {
+            let nodes = JSON.parse(line.dataset.nodes);
+            return nodes.includes(ship.parentNode.id) && nodes.includes(targetNode.id)
+        }
+    ).length > 0);
+}
+
+function issueMoveOrder( ship, targetNode ) {
+    ship.dataset.moveTarget = targetNode.id;
+    ship.classList.add('travelled');
+}
+
+function cancelMoveOrder( ship ) {
+    ship.dataset.moveTarget = null;
+    ship.classList.remove('travelled');
+}
+
+function selectNode( node ) {
+    node.classList.add('selected');
+    for (let connectionLine of getAllConnectionsForNode(node)){
+        connectionLine.classList.add('selected');
+    }
+}
+
+function selectShip( ship ) {
+    deselectShip();
+    ship.classList.add('selected');
+    currentShip = ship;
+    if (ship.dataset.moveTarget) {
+        for (let line of document.getElementsByClassName('connection-line')) {
+            let nodes = JSON.parse(line.dataset.nodes);
+            if (nodes.includes(ship.dataset.moveTarget) && nodes.includes(ship.parentNode.id)) {
+                line.classList.add('travelled');
+                if (ship.dataset.owner != currentPlayer) {
+                    line.classList.add('enemy');
+                }
+            }
+        }
+        for (let node of document.getElementsByClassName('game-node')) {
+            if (node.id == ship.dataset.moveTarget) {
+                node.classList.add('travelled');
+                if (ship.dataset.owner != currentPlayer) {
+                    node.classList.add('enemy');
+                }
+            }
+        }
+    }
+}
+
+function shipIsSelected() {
+    return currentShip != null;
 }
 
 function isGameNode( element ) {
     return element.classList.contains('game-node');
+}
+
+function isShip( element ) {
+    return element.classList.contains('game-ship');
 }
 
 function isFriendlyShip( element ) {
@@ -177,29 +269,73 @@ function isEnemyShip( element ) {
     return element.classList.contains('game-ship') && element.dataset.owner != currentPlayer;
 }
 
+function startOfTurnCleanUp() {
+    updateGameInfo();
+    for (let ship of document.getElementsByClassName('game-ship')) {
+        if (isEnemyShip(ship)) {
+            ship.classList.add('enemy');
+        } else {
+            ship.classList.remove('enemy');
+        }
+    }
+}
+
+function removeAllGamePieces() {
+    let gamePieces = document.getElementsByClassName('game-piece');
+    for (let i = gamePieces.length; i > 0; i--) {
+        gamePieces[i-1].remove();
+    }
+}
+
+function startNewGame() {
+    removeAllGamePieces();
+    // Test game setup
+
+    let node1 = createNode(600,400,200,200, gameBoard ,'game-node-1');
+    let node2 = createNode(500,900,200,200, gameBoard ,'game-node-2');
+    let node3 = createNode(300,500,200,200, gameBoard ,'game-node-3');
+    let node4 = createNode(900,800,200,200, gameBoard ,'game-node-4');
+
+    createShip( 'game-node-1', {image: 'HULL_Bomber.png', name: 'bomber-001', owner: 0}, 'ship-1');
+    createShip( 'game-node-1', {image: 'HULL_Bomber.png', name: 'bomber-002', owner: 0}, 'ship-2');
+    createShip( 'game-node-1', {image: 'HULL_Bomber.png', name: 'bomber-003', owner: 0}, 'ship-3');
+    createShip( 'game-node-2', {image: 'HULL_Bomber.png', name: 'bomber-004', owner: 1}, 'ship-4');
+    createShip( 'game-node-2', {image: 'HULL_Bomber.png', name: 'bomber-005', owner: 1}, 'ship-5');
+    createShip( 'game-node-2', {image: 'HULL_Bomber.png', name: 'bomber-006', owner: 1}, 'ship-6');
+
+    makeNodeConnection(document.getElementById('game-node-1'),document.getElementById('game-node-2'));
+    makeNodeConnection(document.getElementById('game-node-1'),document.getElementById('game-node-3'));
+    makeNodeConnection(document.getElementById('game-node-1'),document.getElementById('game-node-4'));
+    makeNodeConnection(document.getElementById('game-node-2'),document.getElementById('game-node-4'));
+
+    startOfTurnCleanUp();
+}
+
+function endMovePhase() {
+    currentPlayer = (currentPlayer + 1)%playerCount;
+    if (currentPlayer == firstPlayer) {
+        moveShips();
+    }
+    startOfTurnCleanUp();
+    clickUpdate();
+    updateGameInfo();
+}
+function moveShips() {
+    for (let ship of document.getElementsByClassName('game-ship')) {
+        if (ship.dataset.moveTarget) {
+            document.getElementById(ship.dataset.moveTarget).appendChild(ship);
+            ship.removeAttribute('data-move-target');
+            ship.classList.remove('travelled');
+        }
+    }
+}
 
 const emblemsFolder = document.getElementById('flask-info').dataset.emblemsFolder;
 let gameBoard = document.getElementById('game-board');
 gameBoard.addEventListener('click', clickedGameBoardElement);
 let currentPlayer = 0;
+let firstPlayer = 0;
+let currentShip = null;
+let playerCount = 2;
 
-// Test game setup
-
-let node1 = createNode(600,400,200,200, gameBoard ,'game-node-1');
-let node2 = createNode(500,900,200,200, gameBoard ,'game-node-2');
-let node3 = createNode(300,500,200,200, gameBoard ,'game-node-3');
-let node4 = createNode(900,800,200,200, gameBoard ,'game-node-4');
-
-createShip( 'game-node-1', {image: 'HULL_Bomber.png', name: 'bomber-001', owner: 0}, 'ship-1');
-createShip( 'game-node-1', {image: 'HULL_Bomber.png', name: 'bomber-002', owner: 0}, 'ship-2');
-createShip( 'game-node-1', {image: 'HULL_Bomber.png', name: 'bomber-003', owner: 0}, 'ship-3');
-createShip( 'game-node-2', {image: 'HULL_Bomber.png', name: 'bomber-004', owner: 1}, 'ship-4');
-createShip( 'game-node-2', {image: 'HULL_Bomber.png', name: 'bomber-005', owner: 1}, 'ship-5');
-createShip( 'game-node-2', {image: 'HULL_Bomber.png', name: 'bomber-006', owner: 1}, 'ship-6');
-
-makeNodeConnection(document.getElementById('game-node-1'),document.getElementById('game-node-2'));
-makeNodeConnection(document.getElementById('game-node-1'),document.getElementById('game-node-3'));
-makeNodeConnection(document.getElementById('game-node-1'),document.getElementById('game-node-4'));
-makeNodeConnection(document.getElementById('game-node-2'),document.getElementById('game-node-4'));
-
-startOfTurnCleanUp();
+startNewGame();
